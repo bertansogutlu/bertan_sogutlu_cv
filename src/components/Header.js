1e1c44767a77b56bb0b57c684617a1aed4099261
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-12 23:08:30 +0300

    header ilk taslak

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default function Header() {
     return (
         <div className='Header'>
-            <h2>Header</h2>
+            <h1>bertan</h1>
+            <container className='header-container'>
+                <div>
+                    <h2>I am a Fronted<br/>Developer...</h2>
+                    <p>...who likes to craft solid and scalable frontend products with great user experiences.</p>
+                    <div className='buttons'>
+                        <button>Github</button>
+                        <button>Linkedin</button>
+                    </div>
+                </div>
+                <div className='header-img'>
+                    <img src='https://picsum.photos/id/1/350/375' alt='cv-img'/>
+                </div>
+            </container>
         </div>
     )
 }
