ae1f16e79ac7a981bdc3c1f6f87b8eb7113b1632
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-12 22:16:33 +0300

    komponentler kuruldu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import Skills from './components/Skills';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
- Hello
+      <Header />
+      <Skills />
+      <Profile />
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+        <div className='Projects'>
+            <h2>Projects</h2>
+        </div>
+    )
+}
