5d5a523dbaf83e625dd8f253034b15dc3ed5033e
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-13 01:19:12 +0300

    footer ilk taslak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/fontawesome-free-brands";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
-    return (
-        <div className='Footer'>
-            <h2>Footer</h2>
-        </div>
-    )
+  return (
+    <div className="Footer">
+      <h2>Send me a message!</h2>
+      <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
+      <a href="https://www.wikipedia.org/">almilasucode@gmail.com</a>
+      <div className="icons">
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faAt} />
+        <FontAwesomeIcon icon={faInstagram} />
+      </div>
+    </div>
+  );
 }
