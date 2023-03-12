afe1787501ddb1521a5f31905c06df09d6e89d33
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-13 00:03:39 +0300

    skills ilk taslak

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import javascript from '../svg/javascript.svg';
+import react from '../svg/react.svg';
+import redux from '../svg/redux.svg';
+import nodejs from '../svg/nodejs.svg';
+import vscode from '../svg/vscode.svg';
+import figma from '../svg/figma.svg';
 
 export default function Skills() {
     return (
         <div className='Skills'>
             <h2>Skills</h2>
+            <div className='skills-container'>
+                <div className='card'>
+                    <img src={javascript} />
+                    <p>JAVASCRIPT</p>
+                </div>
+                <div className='card'>
+                    <img src={nodejs} />
+                    <p>NODE</p>
+                </div>
+                <div className='card'>
+                    <img src={react} />
+                    <p>REACT</p>
+                </div>
+                <div className='card'>
+                    <img src={vscode} />
+                    <p>VS CODE</p>
+                </div>
+                <div className='card'>
+                    <img src={redux} />
+                    <p>REDUX</p>
+                </div>
+                <div className='card'>
+                    <img src={figma} />
+                    <p>FIGMA</p>
+                </div>
+            </div>
         </div>
     )
 }
