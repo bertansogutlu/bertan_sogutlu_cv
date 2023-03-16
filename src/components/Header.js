393c5c0bdb419c5e9f40397e26b935df859ed376
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-16 21:03:34 +0300

    header css guncellendi

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,28 @@ import { faGithub, faLinkedin } from "@fortawesome/fontawesome-free-brands";
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className="container">
         <h1>bertan</h1>
-        <div className="container">
-          <div className="text">
-            <h2>
-              I am a Fronted
-              <br />
-              Developer...
-            </h2>
-            <p>
-              ...who likes to craft solid and scalable frontend products with
-              great user experiences.
-            </p>
-            <div>
-              <button>
-                <FontAwesomeIcon icon={faGithub} /> Github
-              </button>
-              <button>
-                <FontAwesomeIcon icon={faLinkedin} /> Linkedin
-              </button>
-            </div>
+        <div className="child">
+          <h2>
+            I am a Fronted
+            <br />
+            Developer...
+          </h2>
+          <p>
+            ...who likes to craft solid and scalable frontend products with
+            great user experiences.
+          </p>
+          <div>
+            <button>
+              <FontAwesomeIcon icon={faGithub} /> Github
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faLinkedin} /> Linkedin
+            </button>
           </div>
-          <img src="https://picsum.photos/id/1/350/375" alt="cv-img" />
         </div>
+        <img src="https://picsum.photos/id/1/350/375" alt="cv-img" />
       </div>
     </header>
   );
