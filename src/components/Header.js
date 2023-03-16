4f383e00fc5cbf2928b5b1f6c5778c5f43d50d77
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-16 20:49:05 +0300

    header tag'i olusturuldu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/fontawesome-free-brands';
-
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/fontawesome-free-brands";
 
 export default function Header() {
-    return (
-        <div className='Header'>
-            <h1>bertan</h1>
-            <container className='header-container'>
-                <div className='header-text'>
-                    <h2>I am a Fronted<br/>Developer...</h2>
-                    <p>...who likes to craft solid and scalable frontend products with great user experiences.</p>
-                    <div className='buttons'>
-                        <button><FontAwesomeIcon icon={faGithub}/> Github</button>
-                        <button><FontAwesomeIcon icon={faLinkedin}/> Linkedin</button>
-                    </div>
-                </div>
-                <div className='header-img'>
-                    <img src='https://picsum.photos/id/1/350/375' alt='cv-img'/>
-                </div>
-            </container>
+  return (
+    <header>
+      <div>
+        <h1>bertan</h1>
+        <div className="container">
+          <div className="text">
+            <h2>
+              I am a Fronted
+              <br />
+              Developer...
+            </h2>
+            <p>
+              ...who likes to craft solid and scalable frontend products with
+              great user experiences.
+            </p>
+            <div>
+              <button>
+                <FontAwesomeIcon icon={faGithub} /> Github
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faLinkedin} /> Linkedin
+              </button>
+            </div>
+          </div>
+          <img src="https://picsum.photos/id/1/350/375" alt="cv-img" />
         </div>
-    )
+      </div>
+    </header>
+  );
 }
