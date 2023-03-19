51ea0d560d8a705f2b1298b3ce11754376928913
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-03-19 16:12:43 +0300

    reqres eklendi

--- src/App.js
+++ src/App.js
@@ -6,15 +6,29 @@ import Profile from './components/Profile';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import {data} from './data';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [resData,setResData] = useState(data);
+
+  useEffect(() => {
+    axios.post("https://reqres.in/api/users", data)
+    .then(function (response) {
+      console.log(response.data);
+      setResData(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  },[])
   return (
     <div className="App">
-      <Header header={data.header}/>
-      <Skills skills={data.skills}/>
-      <Profile profile={data.profile}/>
-      <Projects projects={data.projects}/>
-      <Footer footer={data.footer}/>
+      <Header header={resData.header}/>
+      <Skills skills={resData.skills}/>
+      <Profile profile={resData.profile}/>
+      <Projects projects={resData.projects}/>
+      <Footer footer={resData.footer}/>
     </div>
   );
 }
